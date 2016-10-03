31793e8c8d3ea8a6904e3e3702ea4d7ca6f58e44
Carlos Moro <carlos.moro.dam1@iessanandres.com>
2016-10-03 12:53:29 +0200

    actividad0607

---
+++ actividad0495.java
@@ -0,0 +1,7 @@
+public class actividad0495{
+	//actividad 0495 Escribe y ejecuta un programa Java
+	//que muestre tu nombre completo por pantalla.
+	public static void main(String args[]){
+		System.out.println("Carlos Moro Flórez");
+	}
+}
